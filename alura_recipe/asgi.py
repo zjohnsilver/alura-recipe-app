b62dd88f498bfbcd3dceb12da762d6db9fd8fc88
John Silver <johncarlosilver@gmail.com>
2022-02-15 22:24:22 -0300

    refactor: rename app alura_recipe

--- alura_recipe/asgi.py
+++ alura_recipe/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for alura_recipe_app project.
+ASGI config for alura_recipe project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alura_recipe_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alura_recipe.settings")
 
 application = get_asgi_application()
--- alura_recipe/urls.py
+++ alura_recipe/urls.py
@@ -1,4 +1,4 @@
-"""alura_recipe_app URL Configuration
+"""alura_recipe URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
