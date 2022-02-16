c0e1291468f8f78211095f053e7d7e54a7df7901
John Silver <johncarlosilver@gmail.com>
2022-02-16 17:11:26 -0300

    feat: page recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from recipes import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("recipes", views.recipes, name="recipes"),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+# Create your views here.
+def recipes(request):
+    return render(request, "recipes.html")
