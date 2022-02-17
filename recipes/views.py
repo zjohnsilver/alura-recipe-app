ca6c306ea478cf786f3167fe61b8b21f44318a88
John Silver <johncarlosilver@gmail.com>
2022-02-17 20:07:17 -0300

    feat: add data to index.html

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    data = {"recipes_name": {1: "Lasanha", 2: "Yakisoba"}}
+    return render(request, "index.html", data)
 
 
 # Create your views here.
